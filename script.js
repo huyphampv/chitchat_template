68940baba28b199bd8b56fa4a49b1efaa2c17243
huyphampv <153630740+huyphampv@users.noreply.github.com>
2023-12-16 10:10:17 +0700

    init project

---
+++ script.js
@@ -0,0 +1,12 @@
+// function tenHam(thamso) {
+//     console.log(thamso);
+//     return "tien";
+// }
+
+// nhanvien1 = tenHam("Harry Potter");
+// console.log(nhanvien1);
+
+const shipper = () => {
+    return 1000;
+}
+console.log(shipper());
